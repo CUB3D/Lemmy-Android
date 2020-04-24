5f02e1dfec68ff8d82830efaa7d087b8da1e42e0
CUB3D <callumthom11@gmail.com>
2020-04-24 05:02:40 +0100

    Parse json comments

--- app/src/main/java/pw/cub3d/lemmy/core/networking/PostResponse.kt
+++ app/src/main/java/pw/cub3d/lemmy/core/networking/PostResponse.kt
@@ -5,9 +5,9 @@ import pw.cub3d.lemmy.core.networking.PostView
 
 @JsonClass(generateAdapter = true)
 data class PostResponse(
-    val post: PostView
-//    val comments: Array<CommentView>,
-//    val community: CommunityView,
+    val post: PostView,
+    val comments: Array<CommentView>,
+    val community: Community
 //    val moderators: Array<CommunityModeratorView>,
 //    val admins: Array<UserView>
 )
