01c36d8f8a9a082182520e92259fbe5078fbed5d
CUB3D <callumthom11@gmail.com>
2020-04-29 02:56:40 +0100

    Added all mod log types

--- app/src/main/java/pw/cub3d/lemmy/ui/modlog/ModlogFragment.kt
+++ app/src/main/java/pw/cub3d/lemmy/ui/modlog/ModlogFragment.kt
@@ -13,7 +13,8 @@ import androidx.recyclerview.widget.RecyclerView
 import dagger.android.support.AndroidSupportInjection
 
 import pw.cub3d.lemmy.core.data.ModLogEntry
-import pw.cub3d.lemmy.core.networking.ModRemovePostView
+import pw.cub3d.lemmy.core.data.ModLogEntryType
+import pw.cub3d.lemmy.core.networking.*
 import pw.cub3d.lemmy.databinding.FragmentModlogBinding
 import pw.cub3d.lemmy.databinding.ModlogRemovedpostEntryBinding
 import javax.inject.Inject
@@ -65,11 +66,20 @@ class ModLogAdapter(ctx: Context) : RecyclerView.Adapter<RecyclerView.ViewHolder
     }
 
     override fun getItemCount() = entries.size
-    override fun getItemViewType(position: Int) = entries[position].type
+    override fun getItemViewType(position: Int) = entries[position].type.id
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        when(getItemViewType(position)) {
-            0 -> (holder as ModLogRemovedPostViewHolder).bind(entries[position].removePostView)
+        val entry = entries[position]
+        when(entry.type) {
+            ModLogEntryType.REMOVED_POST -> (holder as ModLogRemovedPostViewHolder).bind(entry.removePostView!!)
+            ModLogEntryType.LOCKED_POST -> (holder as ModLogRemovedPostViewHolder).bind(entry.lockedPostView!!)
+            ModLogEntryType.STICKIED_POST -> (holder as ModLogRemovedPostViewHolder).bind(entry.stickiedPostView!!)
+            ModLogEntryType.REMOVED_COMMENTS -> (holder as ModLogRemovedPostViewHolder).bind(entry.removedComments!!)
+            ModLogEntryType.REMOVED_COMMUNITIES -> (holder as ModLogRemovedPostViewHolder).bind(entry.removedCommunity!!)
+            ModLogEntryType.BANNED_FROM_COMMUNITY -> (holder as ModLogRemovedPostViewHolder).bind(entry.bannedFromCommunityView!!)
+            ModLogEntryType.BANNED -> (holder as ModLogRemovedPostViewHolder).bind(entry.bannedView!!)
+            ModLogEntryType.ADDED_TO_COMMUNITY -> (holder as ModLogRemovedPostViewHolder).bind(entry.addedToCommunityView!!)
+            ModLogEntryType.ADDED -> (holder as ModLogRemovedPostViewHolder).bind(entry.addedPostView!!)
         }
     }
 
@@ -81,7 +91,56 @@ class ModLogAdapter(ctx: Context) : RecyclerView.Adapter<RecyclerView.ViewHolder
 
 class ModLogRemovedPostViewHolder(val binding: ModlogRemovedpostEntryBinding): RecyclerView.ViewHolder(binding.root) {
     fun bind(removePostView: ModRemovePostView) {
-        binding.removedPostView = removePostView
+        binding.modUserName = removePostView.mod_user_name
+        binding.reason = removePostView.reason
         binding.modLogRemovedPostAction.text = "Removed post \"${removePostView.post_name}\""
+    }
+
+    fun bind(post: ModLockPostView) {
+        binding.modUserName = post.mod_user_name
+        binding.reason = ""
+        binding.modLogRemovedPostAction.text = "Locked post \"${post.post_name}\""
+    }
+
+    fun bind(post: ModStickiedPostView) {
+        binding.modUserName = post.mod_user_name
+        binding.reason = ""
+        binding.modLogRemovedPostAction.text = "Stickied post in \"${post.community_name}\""
+    }
+
+    fun bind(post: ModRemoveCommentView) {
+        binding.modUserName = post.mod_user_name
+        binding.reason = post.reason
+        binding.modLogRemovedPostAction.text = "Removed comment \"${post.comment_content}\""
+    }
+
+    fun bind(post: ModRemoveCommunityView) {
+        binding.modUserName = post.mod_user_name
+        binding.reason = post.reason
+        binding.modLogRemovedPostAction.text = "Removed community in \"${post.community_name}\""
+    }
+
+    fun bind(post: ModBanFromCommunityView) {
+        binding.modUserName = post.other_user_name
+        binding.reason = post.reason
+        binding.modLogRemovedPostAction.text = "Banned \"${post.other_user_name}\" from \"${post.community_name}\""
+    }
+
+    fun bind(post: ModBanView) {
+        binding.modUserName = post.other_user_name
+        binding.reason = post.reason
+        binding.modLogRemovedPostAction.text = "Banned \"${post.other_user_name}\""
+    }
+
+    fun bind(post: ModAddCommunityView) {
+        binding.modUserName = post.other_user_name
+        binding.reason = ""
+        binding.modLogRemovedPostAction.text = "Added \"${post.other_user_name}\" to \"${post.community_name}\""
+    }
+
+    fun bind(post: ModAddView) {
+        binding.modUserName = post.other_user_name
+        binding.reason = ""
+        binding.modLogRemovedPostAction.text = "Added \"${post.other_user_name}\" as Admin"
     }
 }
